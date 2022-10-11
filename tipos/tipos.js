dcdf127d3a39a30ce5613a4905a7fa0215f674f3
herloncosta <herlon36@gmail.com>
2022-10-10 22:08:07 -0300

    module types

---
+++ tipos/tipos.js
@@ -0,0 +1,4 @@
+"use strict";
+// string
+let nome = "João"; // a partir daqui a variável nome passa a ser do tipo string
+console.log(nome);
---
+++ tipos/tipos.ts
@@ -0,0 +1,3 @@
+// string
+let nome = "João" // a partir daqui a variável nome passa a ser do tipo string
+console.log(nome)
