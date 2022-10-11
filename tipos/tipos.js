60cda6e4a514c74da8629182d8e74bb668a05606
herloncosta <herlon36@gmail.com>
2022-10-11 08:02:06 -0300

    tipos - explícitos, any, array, tuplas, enums

--- tipos/tipos.js
+++ tipos/tipos.js
@@ -2,3 +2,36 @@
 // string
 let nome = "João"; // a partir daqui a variável nome passa a ser do tipo string
 console.log(nome);
+// numbers
+let idade = 10;
+idade = 27.6; // o tipo number pode ser inteiro ou flutuante
+// idade = "ana";
+console.log(idade);
+let possuiHobbies = false;
+// possuiHobbies = 1;
+console.log(possuiHobbies);
+// tipos explícitos
+let minhaIdade; // essa variável pode receber qualquer valor sem gerar erro
+minhaIdade = 20;
+minhaIdade = "42";
+// array
+let hobbies = ["Cozinhar", "Praticar Esportes"];
+console.log(hobbies[0]);
+hobbies = [100, 200, 300];
+console.log(hobbies);
+// tuplas
+// tuplas são usadas para definir tamanho, ordem e tipos de uma lista
+let endereco = ["Av Principal", 99, ""];
+console.log(endereco);
+endereco = ["Rua Principal", 78, "Bloco A"];
+console.log(endereco);
+// enums
+var Cor;
+(function (Cor) {
+    Cor[Cor["Verde"] = 0] = "Verde";
+    Cor[Cor["Amarelo"] = 1] = "Amarelo";
+    Cor[Cor["Roxo"] = 100] = "Roxo";
+    Cor[Cor["Azul"] = 101] = "Azul";
+})(Cor || (Cor = {}));
+let minhaCor = Cor.Azul;
+console.log(minhaCor);
--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -1,3 +1,44 @@
 // string
-let nome = "João" // a partir daqui a variável nome passa a ser do tipo string
-console.log(nome)
+let nome: string = "João"; // a partir daqui a variável nome passa a ser do tipo string
+console.log(nome);
+
+// numbers
+let idade: number = 10;
+idade = 27.6; // o tipo number pode ser inteiro ou flutuante
+// idade = "ana";
+console.log(idade);
+
+let possuiHobbies: boolean = false;
+// possuiHobbies = 1;
+console.log(possuiHobbies);
+
+// tipos explícitos
+let minhaIdade: any; // essa variável pode receber qualquer valor sem gerar erro
+minhaIdade = 20;
+minhaIdade = "42";
+
+// array
+let hobbies: any[] = ["Cozinhar", "Praticar Esportes"];
+console.log(hobbies[0]);
+
+hobbies = [100, 200, 300];
+console.log(hobbies);
+
+// tuplas
+// tuplas são usadas para definir tamanho, ordem e tipos de uma lista
+let endereco: [string, number, string] = ["Av Principal", 99, ""];
+console.log(endereco);
+
+endereco = ["Rua Principal", 78, "Bloco A"];
+console.log(endereco);
+
+// enums
+enum Cor {
+  Verde, // 0
+  Amarelo, // 1
+  Roxo = 100,
+  Azul, // 101
+}
+
+let minhaCor: Cor = Cor.Azul;
+console.log(minhaCor);
