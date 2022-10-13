4edfa0b1ec1397835c038af188b00ffb3884f4b4
herloncosta <herlon36@gmail.com>
2022-10-13 20:08:47 -0300

    implementando métodos priceWithDiscount e description

--- classes/desafioProduto.ts
+++ classes/desafioProduto.ts
@@ -4,16 +4,28 @@
 // Obs 1.: Desconto é opcional (valor padrão)
 // Obs 2.: Criar dois produtos, um com dois (sem desconto) e outro com três parâmetros
 
+// Desafio: Implementar método resumo e precoComDesconto
+// Método precoComDesconto deve retornar o valor - desconto
+// Método resumo deve mostrar preço com desconto
+
 class Product {
     constructor(
         public name: string,
         public price: number,
         public discount: number = 0
     ) {}
+
+    public priceWithDiscount(price: number, discount: number): number {
+        return price - (price * (discount))
+    }
+
+    public description(): void {
+        console.log(`${this.name} custa R$${this.priceWithDiscount(this.price, this.discount)} (${this.discount * 100}% off)`)
+    }
 }
 
 const product1 = new Product("Camisa", 69.90)
-const product2 = new Product("Tênis", 159.90, 0.10)
+const product2 = new Product("Tênis", 160, 0.10)
 
-console.log(product1)
-console.log(product2)
+product1.description()
+product2.description()
