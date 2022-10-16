4a505ca71c41f788b90612d2be38d1f7dfcef74f
herloncosta <herlon36@gmail.com>
2022-10-15 21:35:09 -0300

    inserindo constructor e utilizando super()

--- classes/heranca.ts
+++ classes/heranca.ts
@@ -9,6 +9,10 @@
 import { Carro } from "./classes"
 
 class Ferrari extends Carro {
+    constructor(modelo: string, velocidadeMaxima: number) {
+        super("Ferrari", modelo, velocidadeMaxima) // inferindo marca
+    }
+
     public acelerar(): number {
         return this.alterarVelocidade(20)
     }
@@ -18,7 +22,7 @@ class Ferrari extends Carro {
     }
 }
 
-const f40 = new Ferrari("Ferrari", "F40", 324)
+const f40 = new Ferrari("F40", 324)
 
 console.log(`Marca: ${f40.marca} Modelo: ${f40.modelo}`)
 
