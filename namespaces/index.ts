af27e6341b9631b078cf96f5474a1836a137ad0f
herloncosta <herlon36@gmail.com>
2022-10-17 18:31:53 -0300

    namespace aninhado

--- namespaces/index.ts
+++ namespaces/index.ts
@@ -1,18 +1,20 @@
-namespace Areas {
-    const PI = Math.PI
+namespace Geometria {
+    export namespace Area {
+        const PI = Math.PI
 
-    export function circulo(raio: number): number {
-        return PI * Math.pow(raio, 2)
-    }
+        export function circulo(raio: number): number {
+            return PI * Math.pow(raio, 2)
+        }
 
-    export function retangulo(base: number, altura: number): number {
-        return base * altura
+        export function retangulo(base: number, altura: number): number {
+            return base * altura
+        }
     }
 }
 
 const PI = 3.33 // não é gerado erro por estar em um escopo diferente
 
-const areaCirculo = Areas.circulo(10)
-const areaRetangulo = Areas.retangulo(10, 20)
+const areaCirculo = Geometria.Area.circulo(10)
+const areaRetangulo = Geometria.Area.retangulo(10, 20)
 
 console.log(areaCirculo, areaRetangulo)
