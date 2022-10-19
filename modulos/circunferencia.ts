af837c0297782bcc8ccd50afcbadd4b64b5c8935
herloncosta <herlon36@gmail.com>
2022-10-19 18:19:31 -0300

    modulos

---
+++ modulos/circunferencia.ts
@@ -0,0 +1,4 @@
+export function areaCircunferencia(raio: number): number {
+    const PI: number = 3.14
+    return PI * raio * raio
+}
---
+++ modulos/modulo.ts
@@ -0,0 +1,5 @@
+import { areaCircunferencia } from "./circunferencia"
+import { areaRetangulo } from "./retangulo"
+
+console.log(areaCircunferencia(5))
+console.log(areaRetangulo(9, 5))
