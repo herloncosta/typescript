8c3708a187c91abafd85b9c68cdb40f91aa5fb6d
herloncosta <herlon36@gmail.com>
2022-10-22 17:48:38 -0300

    desafio fila

---
+++ generics/desafioFila.ts
@@ -0,0 +1,39 @@
+class Fila<T> {
+
+    private fila: Array<T>
+
+    constructor(...args: T[]) {
+        this.fila = args
+    }
+
+    entrar(elemento: T): void {
+        this.fila.push(elemento)
+        console.log(`${elemento} entrou na fila!`)
+    }
+
+    proximo(): void {
+        if (this.fila.length > 0) {
+            const atendido = this.fila.pop()
+            console.log(`${atendido} foi atendido!`)
+        } else {
+            console.log("A fila está vazia!")
+        }
+    }
+
+    imprimir(): void {
+        const emFila = this.fila.reduce((acc, elemento) => 
+            acc += `${elemento}, `, "")
+        console.log(`${emFila} estão na fila!`)
+    }
+}
+
+const fila = new Fila<string>("Maria", "Cintia", "Herlon", "Pedro")
+fila.entrar("Diego")
+fila.entrar("João")
+fila.imprimir()
+
+fila.proximo()
+fila.proximo()
+fila.proximo()
+
+fila.imprimir()
